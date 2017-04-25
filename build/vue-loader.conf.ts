959b34354af296ae185a14dea90ce3fa711044f6
Andrew Murphy <andrewm1986@gmail.com>
2017-04-25 19:39:54 +1000

    working!

---
+++ build/vue-loader.conf.ts
@@ -0,0 +1,16 @@
+var utils = require('./utils')
+var config = require('../config')
+var isProduction = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  loaders: Object.assign(
+    { ts: 'vue-ts-loader' },
+    utils.cssLoaders({
+      sourceMap: isProduction ?
+        config.build.productionSourceMap :
+        config.dev.cssSourceMap,
+      extract: isProduction
+    })
+  ),
+  esModule: true,
+}
